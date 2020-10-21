941c4ac21ebc6ac17b311acefc1895dac88e7348
Alfredo Patricius Tarigan <62396803+alfredoptarigan@users.noreply.github.com>
2020-10-21 12:31:03 +0700

    Complete Feature Item

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -4,6 +4,13 @@ import Link from "next/link";
 import Youtube from "react-youtube";
 import courses from "src/constants/api/courses";
 import Header from "src/parts/Header";
+// images file
+import NameTag from "public/images/icon-nametag.svg";
+import Playback from "public/images/icon-playback.svg";
+import Certificate from "public/images/icon-certificate.svg";
+
+// components
+import Feature from "src/parts/Details/Feature";
 
 function DetailsCourse({ data }) {
   console.log(data);
@@ -47,6 +54,37 @@ function DetailsCourse({ data }) {
         </div>
         <div className="container mx-auto z-10 relative">
           <Header></Header>
+        </div>
+      </section>
+
+      <section className="container mx-auto pt-24 relative">
+        <div className="absolute top-0 w-full transform -translate-y-1/2">
+          <div className="w-3/4 mx-auto">
+            <div className="flex justify-between">
+              <Feature
+                data={{
+                  icon: <NameTag className="fill-teal-500" />,
+                  meta: "Student",
+                  value: data?.total_student ?? 0,
+                }}
+              />
+              <Feature
+                data={{
+                  icon: <Playback className="fill-teal-500" />,
+                  meta: "Video",
+                  value: data?.total_video ?? 0,
+                }}
+              />
+              <Feature
+                data={{
+                  icon: <Certificate className="fill-teal-500" />,
+                  meta: "Certificate",
+                  value:
+                    data?.certificate === 1 ? "Tersedia" : "Tidak Tersedia",
+                }}
+              />
+            </div>
+          </div>
         </div>
       </section>
     </>

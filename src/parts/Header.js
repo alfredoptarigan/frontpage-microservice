372ebf3baa1eec4f20729e1513f5eb7773f3a412
Alfredo Patricius Tarigan <62396803+alfredoptarigan@users.noreply.github.com>
2020-10-22 22:23:14 +0700

    Integration Login Page

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import propTypes from "prop-types";
 import { useRouter } from "next/router";
 import Logo from "public/images/logo.svg";
-
+import DefaultAvatar from "public/images/default-avatar.svg";
 export default function Header({ onLight }) {
+  const [User, setUser] = useState(() => null);
+
+  useEffect(() => {
+    const userCookies =
+      decodeURIComponent(window.document.cookie)
+        ?.split(";")
+        ?.find?.((item) => item.indexOf("BWAMICRO:user") > -1)
+        ?.split("=")[1] ?? null;
+    // console.log(JSON.parse(userCookies));
+    setUser(userCookies ? JSON.parse(userCookies) : null);
+  }, []);
+
   const linkColor = onLight ? "text-gray-900" : "text-white";
   const router = useRouter();
   const linkCTA =
@@ -19,7 +31,7 @@ export default function Header({ onLight }) {
       <div style={{ height: 54 }}>
         <Logo className="on-dark"></Logo>
       </div>
-      <ul className="flex">
+      <ul className="flex items-center">
         <li>
           <Link href="/">
             <a
@@ -69,14 +81,36 @@ export default function Header({ onLight }) {
           </Link>
         </li>
         <li>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={linkCTA}
-            className="bg-indigo-700 hover:bg-indigo-800 transition-all duration-200 text-white hover:text-teal-500 text-lg px-6 py-3 font-medium ml-6"
-          >
-            {textCTA}
-          </a>
+          {User ? (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={linkCTA}
+              className="bg-indigo-700 hover:bg-indigo-800 transition-all duration-200 items-center text-white hover:text-teal-500 text-lg px-6 py-3 font-medium ml-6 inline-flex"
+            >
+              <span className="rounded-full overflow-hidden mr-3 border border-2 border-orange-500 ">
+                {User?.thumbnail ? (
+                  <img
+                    src={User?.thumbnail}
+                    alt={User?.name ?? "Username"}
+                    className="object-cover w-8 h-8 inline-block"
+                  />
+                ) : (
+                  <DefaultAvatar className="fill-indigo-500 w-8 h-8 inline-block"></DefaultAvatar>
+                )}
+              </span>
+              Hi,{User.name}
+            </a>
+          ) : (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={linkCTA}
+              className="bg-indigo-700 hover:bg-indigo-800 transition-all duration-200 text-white hover:text-teal-500 text-lg px-6 py-3 font-medium ml-6"
+            >
+              {textCTA}
+            </a>
+          )}
         </li>
       </ul>
     </header>
